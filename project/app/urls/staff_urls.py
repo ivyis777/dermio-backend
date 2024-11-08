d3696e5e83780eaf3e040192fb23b74a21d25262
ivyis777 <contact@ivyisark.com>
2024-11-08 18:56:47 +0530

    top doctors

--- project/app/urls/staff_urls.py
+++ project/app/urls/staff_urls.py
@@ -8,8 +8,10 @@ urlpatterns = [
     path('staff_meta/<int:staff_meta_id>/', staff_meta_data_create_or_update, name='update-staff-meta'),
     path('api/staff-by-department/<str:department>/', get_staff_by_department, name='get_staff_by_department'),
     path('check_availabity/', check_availability, name='check_doctor_availability'),
-    path('top_doctors/', TopDoctorsListCreateAPIView.as_view(), name='top_doctors_list_create'),
-    path('top_doctors/<int:top_doctor_id>/', TopDoctorsDetailAPIView.as_view(), name='top_doctors_detail'),
+    path('top_doctorsc/', TopDoctorsListCreateAPIView.as_view(), name='top_doctors_list_create'),
+    path('top_doctorsc/<int:top_doctor_id>/', TopDoctorsDetailAPIView.as_view(), name='top_doctors_detail'),
+    path('top-doctors/', top_doctors_crud, name='top_doctors_create_list'),  # For create (POST) and list (GET)
+    path('top-doctors/<int:top_doctor_id>/',top_doctors_crud, name='top_doctors_detail'),  # For retrieve (GET), update (PUT), delete (DELETE)
 
 
-]
+]   
