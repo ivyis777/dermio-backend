260d79401899aa1e9e587cf4ee06cc54decbf1aa
ivyis777 <contact@ivyisark.com>
2024-11-13 19:02:34 +0530

    modified patient controller

--- project/app/controllers/patient_controller.py
+++ project/app/controllers/patient_controller.py
@@ -64,18 +64,22 @@ def symptom_detail(request, pk):
         return Response(status=status.HTTP_204_NO_CONTENT)
 
 class BookAppointmentList(APIView):
-    parser_classes = [MultiPartParser, FormParser]  # To handle file uploads
 
-    def post(self, request):
+        parser_classes = [MultiPartParser, FormParser]  # To handle file uploads
+    
+        def post(self, request):
 
-        serializer = BookAppointmentSerializer(data=request.data, context={'request': request})
-        if serializer.is_valid():
-            serializer.save()
-            # create_notification(user_id,
-            #                                 "Quiz_Unsubcription",
-            #                                 f"INR {sub_amount} has been credited to wallet & Updated wallet Balance is {recipient_wallet.wallet_bal}")
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+            serializer = BookAppointmentSerializer(data=request.data, context={'request': request})
+            if serializer.is_valid():
+                serializer.save()
+                # create_notification(user_id,
+                #                                 "Quiz_Unsubcription",
+                #                                 f"INR {sub_amount} has been credited to wallet & Updated wallet Balance is {recipient_wallet.wallet_bal}")
+                # return Response(serializer.data, "status": "400",status=status.HTTP_201_CREATED)
+                return Response({"appointment_data": [serializer.data],"status":"200"}, status=200)
+            
+            else:
+                return JsonResponse({'errors': serializer.errors, "status": "400"}, status=400)
 
 
 
@@ -407,7 +411,7 @@ def login_user(request):
 
             through_google=data.get('through_google') 
            
-            if through_google == "True" :  
+            if   through_google == "True" :  
                 # print("inif") 
                 patient_exists = Patient.objects.filter(email=email,status=True).exists()
 
