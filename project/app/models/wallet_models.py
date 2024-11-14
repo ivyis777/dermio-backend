a3596951359a01fe78868bf436125e9fef115a8a
ivyis777 <contact@ivyisark.com>
2024-11-14 17:48:41 +0530

    wallets

--- project/app/models/wallet_models.py
+++ project/app/models/wallet_models.py
@@ -36,21 +36,21 @@ class wallet_transactions_debit(models.Model):
         managed = True
         db_table='wallet_transacation_debit'
 
-# class wallet_transactions_credit(models.Model):
-#     transaction_id=models.BigAutoField(primary_key=True)
-#     date_time=models.DateTimeField()
-#     amount=models.DecimalField(max_digits=10, decimal_places=2)
-#     is_credit=models.BooleanField()
-#     to=models.CharField(max_length=40,db_column='to')
-#     to_user_id=models.ForeignKey(User,on_delete=models.CASCADE,to_field='id',db_column='to_user_id', db_constraint=False,null=True,blank=True)
-#     from_username=models.CharField(max_length=50,db_column='from_username')
+class wallet_transactions_credit(models.Model):
+    transaction_id=models.BigAutoField(primary_key=True)
+    date_time=models.DateTimeField()
+    amount=models.DecimalField(max_digits=10, decimal_places=2)
+    is_credit=models.BooleanField()
+    to=models.CharField(max_length=40,db_column='to')
+    to_user_id=models.ForeignKey(Patient,on_delete=models.CASCADE,to_field='id',db_column='to_user_id', db_constraint=False,null=True,blank=True)
+    from_username=models.CharField(max_length=50,db_column='from_username')
 
-#     is_from=models.CharField(max_length=50,db_column='from')
-#     current_bal=models.DecimalField(max_digits=10, decimal_places=2,default=0.00) 
+    is_from=models.CharField(max_length=50,db_column='from')
+    current_bal=models.DecimalField(max_digits=10, decimal_places=2,default=0.00) 
 
 
 
-#     class Meta:
-#         managed = True
+    class Meta:
+        managed = True
 
-#         db_table='wallet_transacation_credit'
+        db_table='wallet_transacation_credit'
