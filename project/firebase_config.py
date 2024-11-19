b4087c7ed5009a5f3f57c0b92c6fa60a3613bb45
ivyis777 <contact@ivyisark.com>
2024-11-19 05:25:31 +0000

    firebas integrations

---
+++ project/firebase_config.py
@@ -0,0 +1,8 @@
+import firebase_admin
+from firebase_admin import credentials
+
+# Path to your service account file
+cred = credentials.Certificate('/home/ubuntu/project/quizapp/project/credentials/google-servicess.json')
+firebase_admin.initialize_app(cred)
+
+
