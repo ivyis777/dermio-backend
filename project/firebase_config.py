67c1a91c2314b71d1d3d25de374fa180edeaf526
ivyis777 <contact@ivyisark.com>
2024-11-20 12:48:49 +0530

    added firebase config cred path

--- project/firebase_config.py
+++ project/firebase_config.py
@@ -1,8 +1,8 @@
-# import firebase_admin
-# from firebase_admin import credentials
+import firebase_admin
+from firebase_admin import credentials
 
-# # Path to your service account file
-# cred = credentials.Certificate('/home/ubuntu/project/quizapp/project/credentials/google-servicess.json')
-# firebase_admin.initialize_app(cred)
+# Path to your service account file
+cred = credentials.Certificate('/home/ubuntu/dermio-backend/project/secure-config/google-servicess.json')
+firebase_admin.initialize_app(cred)
 
 
